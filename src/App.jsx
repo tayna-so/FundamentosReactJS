2072db2d6c3d98e468d07892f1f3505a6e0853da
tayna-sousa <tayna.igt@hotmail.com>
2023-10-18 17:49:55 -0300

    updates sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,29 @@
 import { Header } from './components/Header';
-import { Post } from './Post'
+import { Post } from './Post';
 
+import styles from './App.module.css';
 
 import './global.css';
+import { Sidebar } from './components/Sidebar';
 
 export function App() {
   return (
     <div>
       <Header />
-      <Post
-        author="Tayna Sousa"
-        content="abc"
-      />
-      <Post
-        author="Tayna Araujo"
-        content="defgh"
-      />
+  
+      <div className={styles.wrapper}>
+         <Sidebar />
+         <main>
+          <Post
+            author="Tayná Sousa"
+            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta, error alias fugit hic voluptas odit voluptate numquam culpa ipsum iusto cum labore quos a nesciunt reiciendis aperiam unde tempora sapiente?"
+          />
+          <Post
+            author="Tayná Sousa"
+            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta, error alias fugit hic voluptas odit voluptate numquam culpa ipsum iusto cum labore quos a nesciunt reiciendis aperiam unde tempora sapiente?"
+          />
+        </main>
+      </div>
     </div>
   )
 }
